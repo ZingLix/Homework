d5b517b5760636ab75523b161dd7d0da9aa8f970
ZingLix <roadmankael@gmail.com>
2016-11-24 22:47:32 +0800

    P235-4

--- C/Homework/P235-4/P235-4/源.cpp
+++ C/Homework/P235-4/P235-4/源.cpp
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int **a, int **b);
+void swap(int *a, int *b);
 
 int main() {
 	int a, b;
 	scanf("%d%d", &a, &b);
-	int *x = &a, *y = &b;
-	printf("Before the change:a=%d b=%d\n", *x, *y);
-	swap(&x, &y);
-	printf("After the change:a=%d b=%d\n", *x, *y);
+	printf("Before the change:a=%d b=%d\n", a, b);
+	swap(&a, &b);
+	printf("After the change:a=%d b=%d\n", a, b);
 	system("pause");
 	return 0;
 }
 
-void swap(int **a, int **b) {
-	int *tmp = *a;
+void swap(int *a, int *b) {
+	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
