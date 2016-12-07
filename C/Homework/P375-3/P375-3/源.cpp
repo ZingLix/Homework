f5057376275032b4fb2e4bd204a6052662d2fa1a
ZingLix <roadmankael@gmail.com>
2016-12-07 10:35:42 +0800

    1

--- C/Homework/P375-3/P375-3/源.cpp
+++ C/Homework/P375-3/P375-3/源.cpp
@@ -20,28 +20,3 @@ int main() {
 	system("pause");
 	return 0;
 }
-//}
-//#include <stdio.h>   
-//#include <stdlib.h>
-//#define MAX_LINE 1024                   //每行最大字节数  
-//
-//int main()
-//{
-//	FILE *fp;
-//	char strLine[MAX_LINE];                             //读取缓冲区  
-//	if ((fp = fopen("data.txt", "r")) == NULL)      //判断文件是否存在及可读  
-//	{
-//		printf("Open Falied!");
-//		return -1;
-//	}
-//	while (!feof(fp))                                   //循环读取每一行，直到文件尾  
-//	{
-//		fgets(strLine, MAX_LINE, fp);                     //将fp所指向的文件一行内容读到strLine缓冲区  
-//		printf("%s", strLine);                          //输出所读到的内容  
-//														//DO SOMETHING ELSE  
-//	}
-//	fclose(fp);                                         //关闭文件  
-//	printf("\n");
-//	system("pause");
-//	return 0;
-//}
