5b65539dcdbbd584ef7f0fd167848d211e91ffe7
ZingLix <roadmankael@gmail.com>
2018-03-18 19:30:54 +0800

    add time test

--- Data Structures & Algorithm/Homework/Homework 2/src/Main.java
+++ Data Structures & Algorithm/Homework/Homework 2/src/Main.java
@@ -1,24 +1,33 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
-        int upperLimit = 100;
+        int upperLimit = 1000000;
         int k = 3;
 
         ArrayList<Integer> data = new ArrayList<>();
+        System.out.println("Size\tTime1\tTime2");
+        for(Integer size=10;size<100000000;size*=10){
+            // System.out.print("原始数据：");
+            for (int i = 0; i < size; i++) {
+                data.add((int) (Math.random() * upperLimit));
+                //  System.out.print(data.get(i) + " ");
+            }
+            //System.out.println();
+            long start = System.currentTimeMillis();
+            int k1 = Selection.Select1(data, size/2);
+            long end = System.currentTimeMillis();
+            long time1=end-start;
+            start=System.currentTimeMillis();
+            int k2 = Selection.Select2(data, k);
+            end= System.currentTimeMillis();
+            long time2=end-start;
 
-        System.out.print("原始数据：");
-        for (int i = 0; i < 10; i++) {
-            data.add((int) (Math.random() * upperLimit));
-            System.out.print(data.get(i) + " ");
+            System.out.println(data.size()+"\t\t"+time1+"\t\t"+time2);
+           // System.out.println("第" + k + "大：" + k1 + "  Time:"+ time + "  \n");
+            data.clear();
         }
-        System.out.println();
-
-        int k1 = Selection.Select1(data, k);
-        int k2 = Selection.Select2(data, k);
-
-        System.out.println("第" + k + "大：" + k1 + "  " + k2 + "  \n");
-
     }
 }
